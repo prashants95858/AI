47c679dd28e010c732a4cf57273660e7cda13586
Prashant Sanghavi <2000095858@hexaware.com>
2025-09-25 20:10:30 +0530

    Revert "Added test cases for page & layout components."
    This reverts commit 21d289f9cb329a091f8bf9aed49a3ad2b899fbd6.

--- frontend/src/__mocks__/globals.css.js
+++
@@ -1,2 +0,0 @@
-// Manual mock for global CSS import in Jest
-module.exports = {};
--- frontend/src/__mocks__/next_font_google.js
+++
@@ -1,5 +0,0 @@
-// Manual mock for next/font/google for Jest
-module.exports = {
-  Geist: () => ({ variable: 'font-geist-sans' }),
-  Geist_Mono: () => ({ variable: 'font-geist-mono' }),
-};
